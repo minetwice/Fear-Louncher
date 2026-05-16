9816b857ab6d319e9993c7b06f22ff34539bde41
Twicefear <ytd82774@gmail.com>
2026-05-16 12:55:02 +0530

    Update MainActivity.java

--- app/src/main/java/com/fearlauncher/app/MainActivity.java
+++ app/src/main/java/com/fearlauncher/app/MainActivity.java
@@ -1,4 +1,3 @@
-```java
 package com.fearlauncher.app;
 
 import android.os.Bundle;
@@ -11,8 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
     }
+
 }
-```
