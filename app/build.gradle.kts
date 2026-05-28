f6ff50798b45de6c98657331cd24fab17042cad8
Twicefear <ytd82774@gmail.com>
2026-05-28 15:15:56 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,7 +25,6 @@ android {
         }
     }
 
-    // Engine activation for Modern Jetpack Compose UI
     buildFeatures {
         compose = true
     }
@@ -45,18 +44,15 @@ android {
 }
 
 dependencies {
-    // Android Core & Kotlin Environment
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.1")
 
-    // Massive Graphic Engine Components (Compose BOM)
     implementation(platform("androidx.compose:compose-bom:2023.10.01"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
 
-    // Concurrent Worker Framework for Downloading large files
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 }
