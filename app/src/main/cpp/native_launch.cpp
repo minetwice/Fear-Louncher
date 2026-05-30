95a79743a7ec55cc22904ea56905102ab2440d9d
Twicefear <ytd82774@gmail.com>
2026-05-30 23:57:04 +0530

    Update native_launch.cpp

--- app/src/main/cpp/native_launch.cpp
+++ app/src/main/cpp/native_launch.cpp
@@ -25,49 +25,64 @@ Java_com_fearlauncher_launcher_GameLauncher_startMinecraftNative(
     void* handle = dlopen(libjvm_path.c_str(), RTLD_LAZY | RTLD_GLOBAL);
     if (!handle) {
         LOGE("Failed to load libjvm.so: %s", dlerror());
-        goto cleanup;
+        env->ReleaseStringUTFChars(j_jrePath, c_jrePath);
+        env->ReleaseStringUTFChars(j_jarPath, c_jarPath);
+        env->ReleaseStringUTFChars(j_version, c_version);
+        env->ReleaseStringUTFChars(j_assetsPath, c_assetsPath);
+        return -1;
     }
 
     auto createVM = (JNI_CreateJavaVM_t)dlsym(handle, "JNI_CreateJavaVM");
     if (!createVM) {
         LOGE("Failed to find JNI_CreateJavaVM");
-        goto cleanup;
+        dlclose(handle);
+        env->ReleaseStringUTFChars(j_jrePath, c_jrePath);
+        env->ReleaseStringUTFChars(j_jarPath, c_jarPath);
+        env->ReleaseStringUTFChars(j_version, c_version);
+        env->ReleaseStringUTFChars(j_assetsPath, c_assetsPath);
+        return -1;
     }
 
-    // 2. JVM Options - FIX: Store strings in variables first to avoid const_cast error
-    std::string classpath = std::string(c_jarPath) + ":" + 
-                            std::string(c_jrePath) + "/lib/*:" + 
-                            std::string(c_jrePath) + "/lib/ext/*";
+    // 2. JVM Options - FIX: Use proper string storage
+    std::string classpath_str = std::string(c_jarPath) + ":" + 
+                                std::string(c_jrePath) + "/lib/*:" + 
+                                std::string(c_jrePath) + "/lib/ext/*";
+        std::string libPath_str = std::string(c_jrePath) + "/lib";
     
-    std::string libPath = std::string(c_jrePath) + "/lib";
+    // Store option strings in variables to keep them alive
+    std::string opt1_str = "-Djava.class.path";
+    std::string opt4_str = "-Djava.library.path";
     
-    // FIX: Use proper JavaVMOption initialization
-    JavaVMOption opt1, opt2, opt3, opt4;
-    opt1.optionString = const_cast<char*>("-Djava.class.path");
-    opt1.extraInfo = const_cast<char*>(classpath.c_str());
+    JavaVMOption options[4];
+    options[0].optionString = const_cast<char*>(opt1_str.c_str());
+    options[0].extraInfo = const_cast<char*>(classpath_str.c_str());
     
-    opt2.optionString = const_cast<char*>("-Xmx2G");
-    opt2.extraInfo = nullptr;    
-    opt3.optionString = const_cast<char*>("-Xms1G");
-    opt3.extraInfo = nullptr;
+    options[1].optionString = const_cast<char*>("-Xmx2G");
+    options[1].extraInfo = nullptr;
     
-    opt4.optionString = const_cast<char*>("-Djava.library.path");
-    opt4.extraInfo = const_cast<char*>(libPath.c_str());
+    options[2].optionString = const_cast<char*>("-Xms1G");
+    options[2].extraInfo = nullptr;
     
-    JavaVMOption options[] = {opt1, opt2, opt3, opt4};
+    options[3].optionString = const_cast<char*>(opt4_str.c_str());
+    options[3].extraInfo = const_cast<char*>(libPath_str.c_str());
 
     JavaVMInitArgs vm_args;
-    vm_args.version = JNI_VERSION_1_8;  // FIX: Now jni.h is included so this works
+    vm_args.version = JNI_VERSION_1_8;  // FIX: Now defined in jni.h
     vm_args.nOptions = 4;
     vm_args.options = options;
     vm_args.ignoreUnrecognized = JNI_TRUE;
 
-    JavaVM* vm;
-    JNIEnv* jni_env;
+    JavaVM* vm = nullptr;
+    JNIEnv* jni_env = nullptr;
     jint res = createVM(&vm, &jni_env, &vm_args);
-    if (res != JNI_OK) {
+    if (res != JNI_OK || !vm || !jni_env) {
         LOGE("JVM Creation Failed: %d", res);
-        goto cleanup;
+        dlclose(handle);
+        env->ReleaseStringUTFChars(j_jrePath, c_jrePath);
+        env->ReleaseStringUTFChars(j_jarPath, c_jarPath);
+        env->ReleaseStringUTFChars(j_version, c_version);
+        env->ReleaseStringUTFChars(j_assetsPath, c_assetsPath);
+        return -1;
     }
 
     // 3. Call Minecraft Main
@@ -75,28 +90,38 @@ Java_com_fearlauncher_launcher_GameLauncher_startMinecraftNative(
     if (!mainClass) {
         LOGE("net.minecraft.client.main.Main not found");
         vm->DestroyJavaVM();
-        goto cleanup;
+        dlclose(handle);
+        env->ReleaseStringUTFChars(j_jrePath, c_jrePath);
+        env->ReleaseStringUTFChars(j_jarPath, c_jarPath);
+        env->ReleaseStringUTFChars(j_version, c_version);
+        env->ReleaseStringUTFChars(j_assetsPath, c_assetsPath);
+        return -1;
     }
-
     jmethodID mainMethod = jni_env->GetStaticMethodID(mainClass, "main", "([Ljava/lang/String;)V");
     if (!mainMethod) {
         LOGE("main() method not found");
         vm->DestroyJavaVM();
-        goto cleanup;
+        dlclose(handle);
+        env->ReleaseStringUTFChars(j_jrePath, c_jrePath);
+        env->ReleaseStringUTFChars(j_jarPath, c_jarPath);
+        env->ReleaseStringUTFChars(j_version, c_version);
+        env->ReleaseStringUTFChars(j_assetsPath, c_assetsPath);
+        return -1;
     }
 
     // Build minimal args array
     jobjectArray args = jni_env->NewObjectArray(2, jni_env->FindClass("java/lang/String"), nullptr);
-    jni_env->SetObjectArrayElement(args, 0, jni_env->NewStringUTF("--version"));
-    jni_env->SetObjectArrayElement(args, 1, jni_env->NewStringUTF(c_version));
+    if (args) {
+        jni_env->SetObjectArrayElement(args, 0, jni_env->NewStringUTF("--version"));
+        jni_env->SetObjectArrayElement(args, 1, jni_env->NewStringUTF(c_version));
+    }
 
     LOGI("Launching Minecraft via JVM...");
     jni_env->CallStaticVoidMethod(mainClass, mainMethod, args);
 
     // Cleanup
     vm->DestroyJavaVM();
-
-cleanup:    if (handle) dlclose(handle);
+    dlclose(handle);
     env->ReleaseStringUTFChars(j_jrePath, c_jrePath);
     env->ReleaseStringUTFChars(j_jarPath, c_jarPath);
     env->ReleaseStringUTFChars(j_version, c_version);
